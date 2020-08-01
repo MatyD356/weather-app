d7f6f2c81184e6cd6fc3583e8bf7d322402ec2ef
Mateusz <matyd356@gmail.com>
2020-08-01 08:27:34 +0200

    add env file

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.scss';
+import Header from '../Header/Header'
+import Card from '../Card/Card'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <Header />
+      <Card bgTemp="hot" />
+      <Card bgTemp="rainy" />
+      <Card bgTemp="mild" />
+      <Card bgTemp="cold" />
+      <Card bgTemp="storm" />
+
     </div>
   );
 }
