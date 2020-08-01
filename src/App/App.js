c2c04c33e5d99346db04bf70932efcc8de414e64
Mateusz <matyd356@gmail.com>
2020-08-01 12:34:49 +0200

    render card container

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from '../Header/Header'
-import Card from '../Card/Card'
+import CardContainer from '../CardContainer/CardContainer'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <Card bgTemp="hot" />
-      <Card bgTemp="rainy" />
-      <Card bgTemp="mild" />
-      <Card bgTemp="cold" />
-      <Card bgTemp="storm" />
+      <CardContainer />
     </div>
   );
 }
