1b65a7db913bc208ef0904cdb6662dbfe5a9920d
Mateusz <matyd356@gmail.com>
2020-08-01 12:33:50 +0200

    change Card to a stateless component

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,47 +1,12 @@
 import React from 'react'
 import './Card.scss'
 
-class Card extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loadingData: null,
-      data: {}
-    }
-  }
-  async componentDidMount() {
-    this.changeLoding()
-    try {
-      const city = "london"
-      const key = process.env.REACT_APP_NOT_SECRET_CODE;
-      const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`
-      const response = await fetch(url, { mode: 'cors' });
-      const json = await response.json();
-      this.setState({ data: json });
-    } catch (error) {
-      console.log(error)
-    }
-    setTimeout(() => this.changeLoding(), 1000)
-  }
-  changeLoding = () => {
-    this.setState({
-      loadingData: !this.state.loadingData
-    }, () => console.log(this.state))
-  }
-  cardContent = () => {
-
-  }
-  render() {
-    return (
-      <div className={`Card ${this.props.bgTemp}`} >
-        {
-          this.state.loadingData === false ?
-            this.cardContent() :
-            <div className="Card-loading" />
-        }
-      </div >
-    )
-  }
+const Card = (props) => {
+  return (
+    <div className={`Card ${props.bgTemp}`} >
+      {props.loading === true ? <div className="Card-loading" /> : null}
+    </div >
+  )
 }
 
 export default Card
