37005067a1cc552cd5d0f50fbe777b695459c0d6
Mateusz <matyd356@gmail.com>
2020-08-04 18:54:06 +0200

    add changing bg gradients depending on weather

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -21,63 +21,57 @@ const Card = (props) => {
     ]
     return week[new Date(dataArray[i].dt_txt.slice(0, 10)).getDay() - 1];
   }
-  const CardContnet = () => {
-
-    const KelwinToCelsius = (tempKelvin) => {
-      return Math.round(((tempKelvin - 273.15) + Number.EPSILON) * 100) / 100;
-    }
-    const KelwinToFahrenheit = (tempKelvin) => {
-      return Math.round((((tempKelvin * (9 / 5)) - 459.67) + Number.EPSILON) * 100) / 100;
-    }
-    const ChoseIcon = (dataArray) => {
-      switch (dataArray[i].weather[0].main) {
-        case 'Thunderstorm':
-          return [<FlashOnIcon style={{ fontSize: 200 }} />, 'storm']
-        case 'Drizzle':
-          return [<GrainIcon style={{ fontSize: 200 }} />, 'rainy']
-        case 'Rain':
-          return [<GrainIcon style={{ fontSize: 200 }} />, 'rainy']
-        case 'Snow':
-          return [<AcUnitIcon style={{ fontSize: 200 }} />, 'cold']
-        case 'Mist':
-          return [<WavesIcon style={{ fontSize: 200 }} />, 'mild']
-        case 'Clouds':
-          return [<CloudIcon style={{ fontSize: 200 }} />, 'mild']
-        default:
-          let hour = new Date(dataArray[i].dt_txt).getHours()
-          if (hour > 18 || hour < 6) {
-            return [<NightsStayOutlinedIcon style={{ fontSize: 200 }} />, 'cold']
-          }
-          return [<WbSunnyOutlinedIcon style={{ fontSize: 200 }} />, 'hot']
-      }
-    }
-    return (
-      <div>
-        <h2>{data.city.name}</h2>
-        {ChoseIcon(data.list)[0]}
-        <h3>{getDayOfWeek(data.list)}</h3>
-        <p>{data.list[0].dt_txt.slice(0, 10)}</p>
-        <CardTime />
-        <h2>Temp:</h2>
-        <p>{KelwinToCelsius(data.list[i].main.temp)}°C</p>
-        <p>{KelwinToFahrenheit(data.list[i].main.temp)}°F</p>
-        <div>
-        </div>
-      </div >
-    )
+  const KelwinToCelsius = (tempKelvin) => {
+    return Math.round(((tempKelvin - 273.15) + Number.EPSILON) * 100) / 100;
   }
-  const CardTime = () => {
-    return (
-      <div>
-        <button onClick={() => i > 0 ? setI(i - 1) : null} >-</button>
-        <h2>{data.list[i].dt_txt.slice(10, 16)}</h2>
-        <button onClick={() => i < data.list.length - 1 ? setI(i + 1) : null} >+</button>
-      </div>
-    )
+  const KelwinToFahrenheit = (tempKelvin) => {
+    return Math.round((((tempKelvin * (9 / 5)) - 459.67) + Number.EPSILON) * 100) / 100;
+  }
+  const ChoseIcon = (dataArray) => {
+    switch (dataArray[i].weather[0].main) {
+      case 'Thunderstorm':
+        return [<FlashOnIcon style={{ fontSize: 200 }} />, 'storm']
+      case 'Drizzle':
+        return [<GrainIcon style={{ fontSize: 200 }} />, 'rainy']
+      case 'Rain':
+        return [<GrainIcon style={{ fontSize: 200 }} />, 'rainy']
+      case 'Snow':
+        return [<AcUnitIcon style={{ fontSize: 200 }} />, 'cold']
+      case 'Mist':
+        return [<WavesIcon style={{ fontSize: 200 }} />, 'mild']
+      case 'Clouds':
+        return [<CloudIcon style={{ fontSize: 200 }} />, 'mild']
+      default:
+        let hour = new Date(dataArray[i].dt_txt).getHours()
+        if (hour > 18 || hour < 6) {
+          return [<NightsStayOutlinedIcon style={{ fontSize: 200 }} />, 'cold']
+        }
+        return [<WbSunnyOutlinedIcon style={{ fontSize: 200 }} />, 'hot']
+    }
   }
   return (
-    <div className={data ? `Card mild` : `Card hot`} >
-      {props.loading === true ? <div className="Card-loading" /> : data ? CardContnet() : null}
+    <div className={data && !props.loading ? `Card ${ChoseIcon(data.list)[1]}`
+      : `Card`} >
+      {props.loading === true ? <div className="Card-loading" /> : data ?
+        <div>
+          {console.log(data)}
+          <h2>{data.city.name} | {data.city.country}</h2>
+          {ChoseIcon(data.list)[0]}
+          <h3>{getDayOfWeek(data.list)}</h3>
+          <p>{data.list[0].dt_txt.slice(0, 10)}</p>
+          {/* card Time */}
+          <div>
+            <button onClick={() => i > 0 ? setI(i - 1) : null} >-</button>
+            <h2>{data.list[i].dt_txt.slice(10, 16)}</h2>
+            <button onClick={() => i < data.list.length - 1 ? setI(i + 1) : null} >+</button>
+          </div>
+          <h2>Temp:</h2>
+          <p>{KelwinToCelsius(data.list[i].main.temp)}°C</p>
+          <p>{KelwinToFahrenheit(data.list[i].main.temp)}°F</p>
+          <div>
+          </div>
+        </div >
+        : null}
     </div >
   )
 }
