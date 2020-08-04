bdd26c5f9e9b619f7e8e38f2f382e828d8231d96
Mateusz <matyd356@gmail.com>
2020-08-04 12:16:00 +0200

    add icons dependen on weather conditions

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,30 +1,72 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import './Card.scss'
+import WbSunnyOutlinedIcon from '@material-ui/icons/WbSunnyOutlined';
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+import CloudIcon from '@material-ui/icons/Cloud';
+import GrainIcon from '@material-ui/icons/Grain';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import WavesIcon from '@material-ui/icons/Waves';
+import NightsStayOutlinedIcon from '@material-ui/icons/NightsStayOutlined';
 
 const Card = (props) => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
+  const [i, setI] = useState(0);
   useEffect(() => {
     setData(props.data)
   }, [props.data])
-  const getDayOfWeek = () => {
+  const getDayOfWeek = (dataArray) => {
     const week = [
       'Monday', 'Tuesday', 'Wendnesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
     ]
-    return week[new Date(data.list[0].dt_txt.slice(0, 10)).getDay() - 1];
+    return week[new Date(dataArray[i].dt_txt.slice(0, 10)).getDay() - 1];
   }
   const CardContnet = () => {
+
+    const KelwinToCelsius = (tempKelvin) => {
+      return Math.round(((tempKelvin - 273.15) + Number.EPSILON) * 100) / 100;
+    }
+    const KelwinToFahrenheit = (tempKelvin) => {
+      return Math.round((((tempKelvin * (9 / 5)) - 459.67) + Number.EPSILON) * 100) / 100;
+    }
+    const ChoseIcon = (dataArray) => {
+      switch (dataArray[i].weather[0].main) {
+        case 'Thunderstorm':
+          return [<FlashOnIcon style={{ fontSize: 200 }} />, 'storm']
+        case 'Drizzle':
+          return [<GrainIcon style={{ fontSize: 200 }} />, 'rainy']
+        case 'Rain':
+          return [<GrainIcon style={{ fontSize: 200 }} />, 'rainy']
+        case 'Snow':
+          return [<AcUnitIcon style={{ fontSize: 200 }} />, 'cold']
+        case 'Mist':
+          return [<WavesIcon style={{ fontSize: 200 }} />, 'mild']
+        case 'Clouds':
+          return [<CloudIcon style={{ fontSize: 200 }} />, 'mild']
+        default:
+          let hour = new Date(dataArray[i].dt_txt).getHours()
+          if (hour > 18 || hour < 6) {
+            return [<NightsStayOutlinedIcon style={{ fontSize: 200 }} />, 'cold']
+          }
+          return [<WbSunnyOutlinedIcon style={{ fontSize: 200 }} />, 'hot']
+      }
+    }
     return (
       <div>
         <h2>{data.city.name}</h2>
-        <h3>{getDayOfWeek()}</h3>
+        {ChoseIcon(data.list)[0]}
+        <h3>{getDayOfWeek(data.list)}</h3>
         <p>{data.list[0].dt_txt.slice(0, 10)}</p>
         <CardTime />
-      </div>
+        <h2>Temp:</h2>
+        <p>{KelwinToCelsius(data.list[i].main.temp)}°C</p>
+        <p>{KelwinToFahrenheit(data.list[i].main.temp)}°F</p>
+        <div>
+        </div>
+      </div >
     )
   }
   const CardTime = () => {
-    const [i, setI] = useState(0);
     return (
       <div>
         <button onClick={() => i > 0 ? setI(i - 1) : null} >-</button>
@@ -34,7 +76,7 @@ const Card = (props) => {
     )
   }
   return (
-    <div className={`Card ${props.bgTemp}`} >
+    <div className={data ? `Card mild` : `Card hot`} >
       {props.loading === true ? <div className="Card-loading" /> : data ? CardContnet() : null}
     </div >
   )
